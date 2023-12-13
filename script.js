8c310d6f4a3b48872abb6d1c88b081ae2c2debdf
Creativeheaddev <142106516+Creativeheaddev@users.noreply.github.com>
2023-12-13 14:01:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const extendHex = (shortHex) => {
   // write your code here
 let i=shortHex.length-3 , isCapital = false;
 let output = "#";
-while (i<=shortHex.length){
-	let asciiCode + shortHex[i].charCodeAt(0);
+while (i>=0){
+	let asciiCode = shortHex[i].charCodeAt(0);
 		if (asciiCode<=65 && asciiCode >=90){
 			isCapital=true;
 		}
